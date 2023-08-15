aaf54a9b33718c00eedf443fa7d857ee4cf0666d
alexandereversbusch <abusch419@gmail.com>
2023-08-15 17:43:18 -0400

    Get filenanme sorting algorithm working for rskt and arthur deboise sample packs.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,42 @@
 use std::fs;
 use regex::Regex;
-// use std::cmp::Ordering;
+use std::path::PathBuf;
 
 fn main() {
     println!("Hello, World! 🌍");
-    build_xml_with_one_filename_and_save_it_on_my_desktop(
-        // "/Users/alexandereversbusch/Desktop/Downloaded Sample Packs/RSKT/".to_string()
+    match build_xml_with_one_filename_and_save_it_on_my_desktop(
         "/Users/alexandereversbusch/Desktop/Downloaded Sample Packs/Arthur Duboise/loops/".to_string()
-    );
-}
-
-fn build_xml_with_one_filename_and_save_it_on_my_desktop(filepath: String) -> () {
-    let filenames = get_file_names_from_file_path(filepath);
-    for filename in &filenames {
-        println!("filename {:?}", filename);
+        // "/Users/alexandereversbusch/Desktop/Downloaded Sample Packs/RSKT/".to_string()
+    ) {
+        Ok(_) => {},
+        Err(e) => println!("Error: {}", e),
     }
 }
 
-fn get_file_names_from_file_path(filepath: String) -> Vec<String> {
-    let path = std::path::Path::new(&filepath);
+fn build_xml_with_one_filename_and_save_it_on_my_desktop(filepath: String) -> Result<(), String> {
+    let filenames = get_file_names_from_file_path(&filepath)?;
+    for filename in &filenames {
+        println!("filename {:?}", filename);
+    }
+    Ok(())
+}
+
+fn get_file_names_from_file_path(filepath: &str) -> Result<Vec<String>, String> {
+    let path = PathBuf::from(filepath);
     let mut filenames: Vec<String> = Vec::new();
 
-    if let Ok(entries) = fs::read_dir(&path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                if entry.path().is_dir() {
-                    let mut new_path = filepath.clone();
-                    new_path.push_str(entry.file_name().to_str().unwrap());
-                    new_path.push_str("/");
-
-                    let mut new_filenames = get_file_names_from_file_path(new_path);
-                    filenames.append(&mut new_filenames);
-                } else {
-                    if !entry.file_name().to_string_lossy().starts_with('.') {
-                        filenames.push(entry.file_name().to_string_lossy().to_string());
-                    }
-                }
+    let entries = fs::read_dir(&path).map_err(|_| "Error reading directory".to_string())?;
+    for entry in entries {
+        let entry = entry.map_err(|_| "Error reading entry".to_string())?;
+        if entry.path().is_dir() {
+            let new_path = path.join(entry.file_name());
+            let mut new_filenames = get_file_names_from_file_path(new_path.to_str().ok_or("Invalid path")?)?;
+            filenames.append(&mut new_filenames);
+        } else {
+            if !entry.file_name().to_string_lossy().starts_with('.') {
+                filenames.push(entry.file_name().to_string_lossy().to_string());
             }
         }
-    } else {
-        println!("Error reading directory");
     }
 
     filenames.sort_by(|a, b| {
@@ -52,7 +49,7 @@ fn get_file_names_from_file_path(filepath: String) -> Vec<String> {
         })
     });
 
-    filenames
+    Ok(filenames)
 }
 
 fn extract_type(filename: &str) -> String {
@@ -67,11 +64,16 @@ fn extract_type(filename: &str) -> String {
 fn extract_number(filename: &str) -> i32 {
     let re_start = Regex::new(r"^(\d+)").unwrap();
     let re_end = Regex::new(r"(\d+)\.wav$").unwrap();
+    let re_parenthesis = Regex::new(r"\((\d+)\)").unwrap(); // New regex to capture numbers in parentheses
+
     if let Some(cap) = re_start.captures(filename) {
         cap[1].parse().unwrap_or(0)
     } else if let Some(cap) = re_end.captures(filename) {
+        cap[1].parse().unwrap_or(0)
+    } else if let Some(cap) = re_parenthesis.captures(filename) { // Check for numbers in parentheses
         cap[1].parse().unwrap_or(0)
     } else {
         0
     }
 }
+
